e93b78916d6ae3d424832527c5558b8bf4fd4e32
TAN-TRIK <avinashdwivedig@gmail.com>
2021-07-19 23:34:41 +0530

    alien's ship fleet functionality and shooting them added

--- ai_game.py
+++ ai_game.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion():
@@ -19,12 +20,16 @@ class AlienInvasion():
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         while True:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
             
@@ -65,6 +70,8 @@ class AlienInvasion():
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+
+        self.aliens.draw(self.screen)
         pygame.display.flip()
 
 
@@ -80,6 +87,51 @@ class AlienInvasion():
             if bullet.rect.bottom <=0:
                 self.bullets.remove(bullet)
         print(len(self.bullets))
+        self._check_bullet_collisions()
+
+    def _check_bullet_collisions(self):
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()
+
+    def _create_fleet(self):
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2*alien_width)
+        number_aliens_x = available_space_x // (2*alien_width)
+
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3*alien_height) - ship_height)
+        number_rows = available_space_y // (2*alien_height)
+        
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+        
+    def _create_alien(self, alien_number, row_number):
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2*alien_width*alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2*alien.rect.height*row_number
+        self.aliens.add(alien)
+
+    def _update_aliens(self):
+        self._check_fleet_edges()
+        self.aliens.update()
+
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
 
 if __name__ == "__main__":
--- settings.py
+++ settings.py
@@ -7,5 +7,10 @@ class Settings:
 
         self.ship_speed = 1.5
         
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullets_allowed = 9
+
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+
+        self.fleet_direction = 1
